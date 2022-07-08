0859fab4348246d14d8f68c5c86bc8475909529f
molnattila991 <molnarattila991@gmail.com>
2022-07-08 14:12:52 +0200

    Added custom local guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './features/auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;
--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth/auth.service';
-import { LocalStrategy } from './auth/local.strategy';
+import { LocalAuthGuard } from './local-auth.guard';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   imports: [UsersModule, PassportModule],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, LocalAuthGuard]
 })
-export class AuthModule {}
+export class AuthModule { }
