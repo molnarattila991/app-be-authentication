b0b3c310ef845d44b9cd97b48e11468b7d470138
molnattila991 <molnarattila991@gmail.com>
2022-07-12 11:08:39 +0200

    Updated mongoose options

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './features/auth/auth.module';
 import { BusModule } from 'moat-lib-be-pubsub/pub-sub';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { User, UserSchema } from './models/schemas/user.schema';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot(process.env["NEST_MONGODB_CONNECTION_STRING"], <any>{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: "1" }),
+    MongooseModule.forRoot(process.env["NEST_MONGODB_CONNECTION_STRING"],
+      <MongooseModuleOptions>{
+        useNewUrlParser: true, useUnifiedTopology: true, serverApi: "1",
+        dbName: process.env["NEST_MONGODB_DB_NAME"]
+      }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
   ],
   controllers: [
